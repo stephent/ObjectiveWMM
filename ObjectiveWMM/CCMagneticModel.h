b66bb944b56710130d3c20a603614b09ba1e3514
Stephen Trainor <stephen@crookneckconsulting.com>
2024-01-22 16:22:05 -0700

    Update framework header import statement; build settings

--- ObjectiveWMM/CCMagneticModel.h
+++ ObjectiveWMM/CCMagneticModel.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
 
-#import "CCMagneticDeclination.h"
+#import <ObjectiveWMM/CCMagneticDeclination.h>
 
 @interface CCMagneticModel : NSObject
 
